326accb47fcd3ba1bc0c612b7c39ee00e4fd9946
DevTechDelta <107503728+DevTechDelta@users.noreply.github.com>
2022-06-23 15:50:27 -0700

    Add files via upload

--- js/multilintin.js
+++ js/multilintin.js
@@ -4,6 +4,16 @@ AND PRESENT IT
 var allbg = [
     {"pi":"1","prm": "1.jfif","prf": ""},
   ]; */
+  reaidavatar();
+  function reaidavatar(){
+    var w = window.innerWidth;
+      //alert(w)
+    var tmailt = document.getElementById("tmailt");
+
+    if(w < 480){
+      tmailt.style.marginLeft = "-25px";
+    }
+  }
 var allprod = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57];
 var productlength = 57;
 productlist();
